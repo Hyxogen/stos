d2ce279de620e81f4afd75461ce0272b678f3d07
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-10-16 13:08:39 +0200

    update header

--- stos.h
+++ stos.h
@@ -18,44 +18,12 @@
 #define STOS_H
 
 #include <stddef.h>
-#include <libavformat/avformat.h>
-
-struct file_info 
-{
-	AVFormatContext *fctx;
-};
-
-struct subtitle
-{
-	unsigned int start_time;
-	unsigned int end_time;
-	size_t num_text;
-	char **text;
-	int styled;
-};
 
 enum stos_error
 {
-	STOS_SUCCESS,
-	STOS_OUT_OF_MEMORY,
-	STOS_COULD_NOT_OPEN,
-	STOS_NO_INFO,
-	STOS_NO_STREAM,
-	STOS_INVALID_FORMAT,
-	STOS_COULD_NOT_DECODE,
-	STOS_UNSUPPORTED,
-	STOS_END_OF_STREAM,
-	STOS_UNKNOWN
+	STOS_SUCCESS = 0,
+	STOS_EVAL,
+	STOS_ENOMEM
 };
-
-enum stos_error get_file_info(struct file_info *info, const char *url);
-void del_file_info(struct file_info *info);
-
-enum stos_error get_subs(struct subtitle **out, const struct file_info *info,
-			 int stream_idx, size_t *n);
-void del_sub(struct subtitle *sub);
-void del_subs(struct subtitle *sub, size_t n);
-
-const char* stos_get_error(enum stos_error error);
 
 #endif
