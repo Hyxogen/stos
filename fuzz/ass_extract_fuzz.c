8819255d5c3f698418409063399aa88eb991d2d9
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-10-16 15:00:18 +0200

    use persitent mode for fuzzing

--- fuzz/ass_extract_fuzz.c
+++ fuzz/ass_extract_fuzz.c
@@ -5,54 +5,26 @@
 #include <assert.h>
 #include <string.h>
 
-#define BUFFER_SIZE 1024
-
-static char *read_input(FILE *stream)
+__AFL_FUZZ_INIT();
+int main(void)
 {
-	char buffer[BUFFER_SIZE];
-	char *result = NULL;
-	size_t size = 0;
-	size_t count = 0;
-	size_t nread;
+#ifdef __AFL_HAVE_MANUAL_CONTROL
+        __AFL_INIT();
+#endif
+        unsigned char *buf = __AFL_FUZZ_TESTCASE_BUF;
 
-	while ((nread = fread(buffer, 1, BUFFER_SIZE, stream))) {
-		while (count + nread >= size) {
-			size_t new_size = (size + 1) * 2;
-			result = realloc(result, new_size);
-			assert(result != NULL);
-			size = new_size;
-		}
-		memcpy(result + count, buffer, nread);
-		count += nread;
-	}
-	if (count == size) {
-		result = realloc(result, size + 1);
-		assert(result != NULL);
-	}
-	result[count] = '\0';
-	assert(ferror(stdin) == 0);
-	return result;
-}
+        while (__AFL_LOOP(10000)) {
+                int ilen = __AFL_FUZZ_TESTCASE_LEN;
+                if (ilen <= 1)
+                        continue;
+                if (buf == NULL)
+                        continue;
+                char *out = NULL;
+                size_t len;
+                int styled;
 
-int main(int argc, char **argv)
-{
-	char *input = NULL;
-	if (argc == 1) {
-		input = read_input(stdin);
-	} else {
-		FILE *file = fopen(argv[1], "r");
-		assert(file != NULL);
-		input = read_input(file);
-		assert(fclose(file) == 0);
-	}	
-	char *out = NULL;
-	size_t len;
-	int styled;
-	
-	if (input == NULL)
-		return 0;
-	stos_ass_extract(&out, &len, &styled, input);
-	free(input);
-	free(out);
-	return 0;
+                stos_ass_extract(&out, &len, &styled, buf);
+                free(out);
+        }
+        return 0;
 }
