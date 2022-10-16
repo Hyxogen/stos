b7329bdf2187bdbf9304304f69d402d75e4450aa
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-10-16 16:12:28 +0200

    add more tests for stos_ass_extract

--- test/ass/extract.c
+++ test/ass/extract.c
@@ -1,5 +1,6 @@
 #include <sub_new.c>
 
+#include <stddef.h>
 #include <string.h>
 #include <assert.h>
 
@@ -14,7 +15,38 @@ int main(void)
 		       STOS_SUCCESS);
 		assert(strcmp(text, "息を合わせて…") == 0);
 		assert(styled == 0);
+		assert(len == strlen(text));
 		free(text);
+	}
+	{
+		assert(stos_ass_extract(NULL, NULL, NULL,
+					"348,0,Default,,0,0,0,,息を合わせて…") ==
+		       STOS_SUCCESS);
+	}
+	{
+		size_t len;
+		assert(stos_ass_extract(NULL, &len, NULL,
+					"348,0,Default,,0,0,0,,息を合わせて…") ==
+		       STOS_SUCCESS);
+		assert(len == strlen("息を合わせて…"));
+	}
+	{
+		char *text;
+		size_t len;
+		assert(stos_ass_extract(&text, &len, NULL,
+					"348,0,Default,,0,0,0,,") ==
+		       STOS_SUCCESS);
+		assert(len == 0);
+		assert(*text == '\0');
+		free(text);
+	}
+	{
+		int styled;
+		assert(stos_ass_extract(
+			       NULL, NULL, &styled,
+			       "348,0,Default,,0,0,0,,{\\i1}Hello There{\\i0}") ==
+		       STOS_SUCCESS);
+		assert(styled == 1);
 	}
 	return 0;
 }
