1000d720f629cf34629c2b538963b1507f30eb09
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-10-16 15:46:47 +0200

    format code

--- test/ass/extract.c
+++ test/ass/extract.c
@@ -3,12 +3,15 @@
 #include <string.h>
 #include <assert.h>
 
-int main(void) {
+int main(void)
+{
 	{
 		char *text;
 		size_t len;
 		int styled;
-		assert(stos_ass_extract(&text, &len, &styled, "348,0,Default,,0,0,0,,息を合わせて…") == STOS_SUCCESS);
+		assert(stos_ass_extract(&text, &len, &styled,
+					"348,0,Default,,0,0,0,,息を合わせて…") ==
+		       STOS_SUCCESS);
 		assert(strcmp(text, "息を合わせて…") == 0);
 		assert(styled == 0);
 		free(text);
