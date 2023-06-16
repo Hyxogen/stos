d702a6c11a3c3bd3bc36f1979140ea1b5cb8b35e
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2023-06-16 19:19:06 +0200

    begin refactor

---
+++ src/args.rs
@@ -0,0 +1,113 @@
+use anyhow::Result;
+use log::LevelFilter;
+use std::path::PathBuf;
+
+pub struct Args {
+    pub sub_files: Vec<PathBuf>,
+    pub sub_stream: Option<usize>,
+
+    pub media_files: Vec<PathBuf>,
+
+    pub gen_audio: bool,
+    pub audio_stream: Option<usize>,
+    pub audio_format: String,
+
+    pub gen_image: bool,
+    pub image_format: String,
+    pub verbosity: LevelFilter,
+}
+
+impl Default for Args {
+    fn default() -> Self {
+        Self {
+            sub_files: Default::default(),
+            sub_stream: Default::default(),
+            media_files: Default::default(),
+            gen_audio: false,
+            audio_stream: Default::default(),
+            audio_format: "out_%f_%s.mka".to_string(),
+            gen_image: false,
+            image_format: "out_%f_%s.jpg".to_string(),
+            verbosity: LevelFilter::Error,
+        }
+    }
+}
+
+impl Args {
+    pub fn parse_from_env() -> Result<Self> {
+        use lexopt::prelude::*;
+
+        let mut args = Args::default();
+        let mut subtitles = true;
+
+        let mut parser = lexopt::Parser::from_env();
+        while let Some(arg) = parser.next()? {
+            match arg {
+                Short('m') | Long("media") => {
+                    subtitles = false;
+                }
+                Short('s') | Long("sub-stream") => {
+                    args.sub_stream = Some(parser.value()?.parse()?);
+                }
+                Short('a') | Long("audio") => {
+                    args.gen_audio = true;
+                }
+                Long("audio-stream") => {
+                    args.audio_stream = Some(parser.value()?.parse()?);
+                }
+                Long("audio-format") => {
+                    args.audio_format = if let Ok(format) = parser.value()?.into_string() {
+                        format
+                    } else {
+                        eprintln!("Failed to parse \"--audio-format\" option: Invalid unicode");
+                        std::process::exit(1);
+                    }
+                }
+                Short('i') | Long("image") => {
+                    args.gen_image = true;
+                }
+                Long("image-format") => {
+                    args.image_format = if let Ok(format) = parser.value()?.into_string() {
+                        format
+                    } else {
+                        eprintln!("Failed to parse \"--image-format\" option: Invalid unicode");
+                        std::process::exit(1);
+                    }
+                }
+                Short('v') => {
+                    args.verbosity = LevelFilter::Warn;
+
+                    if let Some(val) = parser.optional_value() {
+                        args.verbosity = match val.into_string().as_deref() {
+                            Ok("v") => LevelFilter::Info,
+                            Ok("vv") => LevelFilter::Debug,
+                            Ok("vvv") => LevelFilter::Trace,
+                            Ok(val) => {
+                                eprintln!(
+                                    "\"{}\" is not a valid value for the verbosity flag \"-v\"",
+                                    val
+                                );
+                                std::process::exit(1);
+                            }
+                            Err(val) => {
+                                eprintln!(
+                                    "Failed to parse verbosity option: Invalid unicode: {}",
+                                    val.to_string_lossy()
+                                );
+                                std::process::exit(1);
+                            }
+                        }
+                    }
+                }
+                Value(val) if subtitles => {
+                    args.sub_files.push(val.into());
+                }
+                Value(val) if !subtitles => {
+                    args.media_files.push(val.into());
+                }
+                _ => panic!(),
+            }
+        }
+        Ok(args)
+    }
+}
--- src/subtitle.rs
+++ src/subtitle.rs
@@ -1,236 +1,108 @@
-extern crate ffmpeg_next as ffmpeg;
+use anyhow::{Context, Error, Result};
+use libav::{codec, decoder, media, codec::subtitle, codec::packet};
+use libav::util::rational::Rational;
+use log::{debug, trace};
+use std::path::PathBuf;
 
-use ffmpeg::codec::{packet, subtitle};
-use ffmpeg::mathematics::rescale::Rescale;
-use ffmpeg::util::rational::Rational;
-use std::fmt;
-
-const ONE_BILLIONTH: Rational = Rational(1, 1000000000);
-
-#[derive(Debug)]
-pub enum SubtitleError {
-    NegativeStart,
-    NegativeEnd,
-    MissingTimestamp,
-    ZeroLength,
-}
-
-impl fmt::Display for SubtitleError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match *self {
-            SubtitleError::NegativeStart => write!(f, "The subtitle start timestamp is negative"),
-            SubtitleError::NegativeEnd => write!(f, "The subtitle end timestamp is negative"),
-            SubtitleError::MissingTimestamp => write!(
-                f,
-                "The subtitle is missing either the start or end timestamp"
-            ),
-            SubtitleError::ZeroLength => write!(f, "The subtitle start and end are the same"),
-        }
-    }
-}
-
-impl std::error::Error for SubtitleError {}
-
-#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub enum Rect {
     Text(String),
 }
 
-impl From<subtitle::Rect<'_>> for Rect {
-    fn from(rect: subtitle::Rect) -> Self {
-        match rect {
-            subtitle::Rect::Text(text) => Rect::Text(text.get().to_string()),
-            subtitle::Rect::Ass(ass) => Rect::Text(ass.get().to_string()),
-            subtitle::Rect::Bitmap(_) => Rect::Text("".to_string()),
-            _ => todo!(),
-        }
+pub struct Timestamp(i64);
+
+impl Timestamp {
+    pub fn from_timebase(ts: i64, time_base: Rational) -> Self {
+        todo!()
     }
 }
 
-#[derive(Default, Debug, Clone, Eq, PartialEq, Hash)]
 pub struct Subtitle {
     rects: Vec<Rect>,
-    pub start: i64,
-    pub end: i64,
-}
-
-impl Extend<Rect> for Subtitle {
-    fn extend<T: IntoIterator<Item = Rect>>(&mut self, iter: T) {
-        self.rects.extend(iter);
-    }
+    pub start: Timestamp,
+    pub end: Timestamp,
 }
 
 impl Subtitle {
-    pub fn new(
-        subtitle: &subtitle::Subtitle,
-        packet: &packet::Packet,
-        time_base: Rational,
-    ) -> Result<Self, SubtitleError> {
-        let start = packet
-            .pts()
-            .or(packet.dts())
-            .ok_or(SubtitleError::MissingTimestamp)?
-            + Into::<i64>::into(subtitle.start()).rescale(ONE_BILLIONTH, time_base);
+    pub fn convert_subtitle(sub: &subtitle::Subtitle, packet: &packet::Packet, time_base: Rational) -> Result<Self> {
+        let start = packet.pts().or(packet.dts()).ok_or(Error::msg("Subtitle is missing a timestamp"))?;
+        let end = start + packet.duration();
 
-        let end = start
-            + packet.duration()
-            + Into::<i64>::into(subtitle.end()).rescale(ONE_BILLIONTH, time_base);
-
-        if start < 0 {
-            Err(SubtitleError::NegativeStart)
-        } else if end < 0 {
-            Err(SubtitleError::NegativeEnd)
-        } else if start == end {
-            Err(SubtitleError::ZeroLength)
-        } else {
-            Ok(Self {
-                start: start.min(end),
-                end: end.max(start),
-                rects: subtitle.rects().map(From::from).collect(),
-            })
-        }
-    }
-
-    pub fn overlaps(&self, other: &Self) -> bool {
-        !(self.end < other.start || other.end < self.start)
-    }
-
-    pub fn iter(&self) -> impl Iterator<Item = &Rect> {
-        self.rects.iter()
+        let start = Timestamp::from_timebase(start, time_base);
+        let end = Timestamp::from_timebase(end, time_base);
+        todo!()
     }
 }
 
-#[derive(Clone, Debug)]
-pub struct SubtitleList {
-    subs: Vec<Subtitle>,
-    pub time_base: Rational,
-}
-
-impl std::ops::Index<usize> for SubtitleList {
-    type Output = Subtitle;
-
-    fn index(&self, index: usize) -> &Self::Output {
-        &self.subs[index]
+fn decode_subtitle(
+    decoder: &mut decoder::subtitle::Subtitle,
+    packet: &codec::packet::packet::Packet,
+) -> Result<Option<subtitle::Subtitle>> {
+    let mut subtitle = Default::default();
+    match decoder.decode(packet, &mut subtitle).context("Failed to decode subtitle")? {
+        true => Ok(Some(subtitle)),
+        false => Ok(None),
     }
 }
 
-impl SubtitleList {
-    pub fn new(time_base: Rational) -> Self {
-        Self {
-            subs: Vec::default(),
-            time_base,
+pub fn read_subtitles(file: &PathBuf, stream_idx: Option<usize>) -> Result<Vec<Subtitle>> {
+    let file_str = file.to_string_lossy();
+
+    let mut ictx = libav::format::input(file).context("Failed to open file")?;
+    trace!("Opened {}", file_str);
+
+    let sub_stream = if let Some(stream_idx) = stream_idx {
+        ictx.streams().nth(stream_idx).ok_or_else(|| {
+            Error::msg(format!(
+                "The file does not have {} streams (has {})",
+                stream_idx,
+                ictx.nb_streams()
+            ))
+        })?
+    } else {
+        ictx.streams()
+            .best(media::Type::Subtitle)
+            .ok_or(Error::msg("The file does not have a subtitle stream"))?
+    };
+
+    let stream_index = sub_stream.index();
+    let codec = sub_stream.parameters().id();
+
+    debug!(
+        "{}: Using subtitle stream at index {}. Codec type: {}",
+        file_str,
+        stream_index,
+        codec.name()
+    );
+
+    let context = codec::context::Context::from_parameters(sub_stream.parameters())
+        .with_context(|| format!("Failed to create codec context for {} codec", codec.name()))?;
+    trace!(
+        "{}: Created codec context for {} codec type",
+        file_str,
+        codec.name()
+    );
+
+    let mut decoder = context.decoder().subtitle().with_context(|| {
+        format!(
+            "{}: Failed to open decoder for {} codec type",
+            file_str,
+            codec.name()
+        )
+    })?;
+    trace!("{}: Opened {} decoder", file_str, codec.name());
+
+    let mut subs = Vec::new();
+
+    for (stream, packet) in ictx.packets() {
+        if stream.index() != stream_index {
+            continue;
+        }
+
+        if let Some(sub) = decode_subtitle(&mut decoder, &packet)? {
+            subs.push(sub);
         }
     }
+    debug!("{}: Read {} subtitle(s)", file_str, subs.len());
 
-    pub fn add_sub(&mut self, sub: Subtitle) -> &mut Self {
-        self.subs.push(sub);
-        self
-    }
-
-    pub fn iter(&self) -> impl Iterator<Item = &Subtitle> {
-        self.subs.iter()
-    }
-
-    pub fn vec(&self) -> &Vec<Subtitle> {
-        &self.subs
-    }
-
-    pub fn len(&self) -> usize {
-        self.subs.len()
-    }
-
-    pub fn coalesce(self) -> Self {
-        let mut subs = Vec::new();
-
-        for subtitle in self.subs {
-            if subs.is_empty() {
-                subs.push(subtitle);
-            } else {
-                let last_idx = subs.len() - 1;
-                let prev_subtitle = &mut subs[last_idx];
-
-                if !prev_subtitle.overlaps(&subtitle) {
-                    subs.push(subtitle);
-                } else {
-                    prev_subtitle.start = std::cmp::min(prev_subtitle.start, subtitle.start);
-                    prev_subtitle.end = std::cmp::max(prev_subtitle.end, subtitle.end);
-                    prev_subtitle.extend(subtitle.rects);
-                }
-            }
-        }
-
-        Self {
-            subs,
-            time_base: self.time_base,
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn no_overlap() {
-        let a = Subtitle {
-            start: 100,
-            end: 1000,
-            ..Default::default()
-        };
-        let b = Subtitle {
-            start: 5000,
-            end: 5100,
-            ..Default::default()
-        };
-        assert_eq!(a.overlaps(&b), false);
-        assert_eq!(b.overlaps(&a), false);
-    }
-
-    #[test]
-    fn partial_overlap() {
-        let a = Subtitle {
-            start: 100,
-            end: 1000,
-            ..Default::default()
-        };
-        let b = Subtitle {
-            start: 500,
-            end: 5000,
-            ..Default::default()
-        };
-        assert_eq!(a.overlaps(&b), true);
-        assert_eq!(b.overlaps(&a), true);
-    }
-
-    #[test]
-    fn exact_overlap() {
-        let a = Subtitle {
-            start: 100,
-            end: 1000,
-            ..Default::default()
-        };
-        let b = Subtitle {
-            start: 100,
-            end: 1000,
-            ..Default::default()
-        };
-        assert_eq!(a.overlaps(&b), true);
-        assert_eq!(b.overlaps(&a), true);
-    }
-
-    #[test]
-    fn complete_overlap() {
-        let a = Subtitle {
-            start: 200,
-            end: 900,
-            ..Default::default()
-        };
-        let b = Subtitle {
-            start: 100,
-            end: 1000,
-            ..Default::default()
-        };
-        assert_eq!(a.overlaps(&b), true);
-        assert_eq!(b.overlaps(&a), true);
-    }
+    Ok(subs)
 }
